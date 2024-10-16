5c24c9c591d674de92bd1ae96357ca711c73e4ab
cameron <cjcombariza@loyola.edu>
2024-10-15 20:07:42 -0400

    i finished the main.py, sorry for not commiting it earlier if forgot i had to do that

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+balance = 1000
+
+choice = 'g'
+SENTINEL = 'e'
+
+while choice != SENTINEL:
+    choice = input("would you like to do: withdraw (w), deposit (d), view balance (v), or exit (e)").lower()
+    if choice == "w":
+        withdraw == int(input("how much would you like to withdraw: "))
+        if amount < 0:
+            print("you can't deposit a negative amount")
+        balance -= withdraw
+        if balance < 0:
+            print("you will be charged 5% interest")
+        print("\nyour current balance is", balance, "\n")
+    if choice == "d":
+        deposit == int(input("how much would you like to deposit: "))
+        if deposit < 0:
+            print("you can't withdraw a negative amount")
+        balance += deposit
+        print("\nyour current balance is", balance, "\n")
+    if choice == "v":
+        print("your balance is", balance, "\n")
+if choice == SENTINEL:
+    print("Thank you for using this ATM, \nhave a good day")
